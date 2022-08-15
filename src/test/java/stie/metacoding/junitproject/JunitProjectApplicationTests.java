64b5d705b36e4272ef54771352c4a770d74dc44c
futuremaker019 <futuremaker019@gmail.com>
2022-08-15 16:27:04 +0900

    initial project

---
+++ src/test/java/stie/metacoding/junitproject/JunitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package stie.metacoding.junitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
